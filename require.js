21b15df10f14b9d3a11c6ab0856a1b43b5c3ade2
Wagner Pedro <wagnerpedro355@gmail.com>
2023-05-20 22:53:22 -0700

    first commit

---
+++ require.js
@@ -0,0 +1,9 @@
+//modulos nativos do node
+// const path = require('path')
+// console.log(path.basename('users/content/popcorn.jsx'))
+
+//meus módulos
+
+const myModule = require('./exports')
+
+console.log(myModule)
