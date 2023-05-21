452e059c1c515d480d624d0eddce60b01bcae7e7
Wagner Pedro <wagnerpedro355@gmail.com>
2023-05-20 23:04:15 -0700

    break

--- process.js
+++ process.js
@@ -1,1 +1,7 @@
-console.log(process)
+//lista de argumentos
+console.log(process.argv)
+
+const firstName = process.argv[2]
+const secondName = process.argv[3]
+
+console.log(`O seu nome é ${firstName} ${secondName}`) 
